7a52b0115ac1493fd919ef4cd75367c03f950b06
Albert Hernandez Jr <albert.hernandez.jr@outlook.com>
2019-06-19 00:59:39 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.Write("Good day, " + name + "!");
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
